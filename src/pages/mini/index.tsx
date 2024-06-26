caaae452c1d5a531799136f4560c3643228a28b6
Ignat Sychev <amanewave@gmail.com>
2024-06-26 16:02:13 +1000

    git commit -m потом доделаю

--- src/pages/mini/index.tsx
+++ src/pages/mini/index.tsx
@@ -1,4 +1,61 @@
 import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
+import VALERA from '../../assets/valera_default.png'
+import { $priceFeed, addScore, isDevelopment } from '../../shared/config/game'
 
-export const Games = () => {};
+const Wrapper  = styled.div`
+    display:flex;
+    flex-direction:row;
+    gap:40px;
+    justify-content:center;
+`
+const ShowCase = styled.div`
+    display:flex;
+    flex-direction:column;
+    background-color:green;
+    height:90vh;
+    width:60vh;
+    gap:2px;
+`
+
+const ShowCaseHeader = styled.div`
+    border:3px solid black;
+    display:flex;
+    justify-content:center;
+`
+
+const ShowCasePic = styled.div`
+    
+`
+
+const ShowCaseMoney = styled.div`
+	border: 3px solid black;
+	display: flex;
+	justify-content: center;
+`
+
+
+export const Games = () => {
+
+
+    const random_money = Math.floor(Math.random() * 100000)
+
+
+
+    return (
+			<div>
+				<Wrapper>
+					<ShowCase>
+						<ShowCaseHeader>{'valerka'}</ShowCaseHeader>
+						<ShowCasePic> valerka img</ShowCasePic>
+						<ShowCaseMoney>{random_money}</ShowCaseMoney>
+					</ShowCase>
+					<ShowCase>
+						<ShowCaseHeader>{'enemy_name'}</ShowCaseHeader>
+						<ShowCasePic>enemy img</ShowCasePic>
+						<ShowCaseMoney>{random_money}</ShowCaseMoney>
+					</ShowCase>
+				</Wrapper>
+			</div>
+		)
+};
