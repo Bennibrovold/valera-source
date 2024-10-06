e85b43c74f27919346cfb69ebc58e9fc58736ef4
Hellmorphin <danilmosenkov@yandex.ru>
2024-10-07 09:27:25 +1000

    feat: up

--- src/features/game-history/index.tsx
+++ src/features/game-history/index.tsx
@@ -18,7 +18,7 @@ export const GameHistory = () => {
       }).then((x) => {
         setHistoryScore(HYSTORY[rand_val].bonus);
       });
-    }, 400000);
+    }, 500000);
   }, []);
 
   return null;
